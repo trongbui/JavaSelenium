2f588bd085ce24e22b16883959de08219f0cb17c
TrongBui <ductrong.bui@gmail.com>
2021-11-16 22:07:32 +0700

    Update TestNGTest.java

--- src/test/java/com/itms/training/java/selenium/tests/TestNGTest.java
+++ src/test/java/com/itms/training/java/selenium/tests/TestNGTest.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -57,4 +58,91 @@ public class TestNGTest {
         driver.close();
     }
 
+    @Test
+    public void createAccount() throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://automationpractice.com/index.php");
+
+        WebElement btnSignIn = driver.findElement(By.className("login"));
+        btnSignIn.click();
+
+        WebElement emailTextbox = driver.findElement(By.id("email_create"));
+        emailTextbox.sendKeys("dannt2@yopmail.com");
+
+        WebElement btnSubmitEmail = driver.findElement(By.xpath("//*[@id = 'SubmitCreate']"));
+        btnSubmitEmail.click();
+        Thread.sleep(5000);
+
+//        WebElement header = driver.findElement(By.tagName("h1"));
+//        String actualHeading = header.getText();
+//
+//        String expectHeading = "CREATE AN ACCOUNT";
+//
+//        Assert.assertEquals(actualHeading, expectHeading);
+
+        WebElement chkTitle = driver.findElement(By.id("id_gender2"));
+        chkTitle.click();
+
+        WebElement FirstnameTextbox = driver.findElement(By.id("customer_firstname"));
+        FirstnameTextbox.sendKeys("Tam Dan");
+
+        WebElement LastnameTextbox = driver.findElement(By.name("customer_lastname"));
+        LastnameTextbox.sendKeys("Nguyen");
+
+        WebElement Pwd = driver.findElement(By.id("passwd"));
+        Pwd.sendKeys("Galaxy@2020");
+
+        WebElement dobDays = driver.findElement(By.id("days"));
+        Select slDays = new Select(dobDays);
+        slDays.selectByIndex(5);
+
+        WebElement dobMonths = driver.findElement(By.id("months"));
+        Select slMonths = new Select(dobMonths);
+        slMonths.selectByValue("5");
+
+        Select slYears = new Select(driver.findElement(By.id("years")));
+        slYears.selectByVisibleText("2010  ");
+
+        WebElement Company = driver.findElement(By.id("company"));
+        Company.sendKeys("Company Testing");
+
+        WebElement CompanyAdd1 = driver.findElement(By.id("address1"));
+        CompanyAdd1.sendKeys("Company Testing Address");
+
+        WebElement CompanyAdd2 = driver.findElement(By.id("address2"));
+        CompanyAdd2.sendKeys("7 floor");
+
+        WebElement City = driver.findElement(By.id("city"));
+        City.sendKeys("Ha Noi");
+
+        Select slStates = new Select(driver.findElement(By.id("id_state")));
+        slStates.selectByVisibleText("Florida");
+
+        WebElement Postcode = driver.findElement(By.name("postcode"));
+        Postcode.sendKeys("700000");
+
+        WebElement Others = driver.findElement(By.id("other"));
+        Others.sendKeys("\"Something to input here.\"");
+
+        WebElement HomePhone = driver.findElement(By.id("phone"));
+        HomePhone.sendKeys("0123456789");
+
+        WebElement MobilePhone = driver.findElement(By.xpath("//*[@name = 'phone_mobile']"));
+        MobilePhone.sendKeys("+84111222333");
+
+        WebElement AddressAlias = driver.findElement(By.xpath("//*[@name = 'alias']"));
+        AddressAlias.sendKeys(" For future reference");
+    }
+
+    @Test
+    public void testLocators() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.airbnb.com/");
+
+        WebElement elPlacesToStays = driver.findElement(By.xpath("//button[.='Experiences']"));
+
+
+    }
 }
